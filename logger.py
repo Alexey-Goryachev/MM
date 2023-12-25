b1cc158b758e4293137d852f9793de2a79c6fd5e
Alexey-Goryachev <goryachev.alex00@gmail.com>
2023-12-25 21:46:33 +0200

    Update logger.py

--- logger.py
+++ logger.py
@@ -1,6 +1,40 @@
+import logging
+from datetime import datetime
 
 
+def setup_logger(log_file_path):
+    # Создаем объект логгера
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
-def log_to_file(message):
-    # Логирование сообщения в файл
-    pass
+    # Создаем форматтер
+    formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+
+    # Создаем файловый обработчик
+    file_handler = logging.FileHandler(log_file_path)
+    file_handler.setLevel(logging.DEBUG)
+    file_handler.setFormatter(formatter)
+
+    # Создаем консольный обработчик
+    console_handler = logging.StreamHandler()
+    console_handler.setLevel(logging.DEBUG)
+    console_handler.setFormatter(formatter)
+
+    # Добавляем обработчики к логгеру
+    logger.addHandler(file_handler)
+    logger.addHandler(console_handler)
+
+    return logger
+
+
+# Пример использования
+logger = setup_logger("bot_log.txt")
+
+# Ваш основной код...
+
+# Пример логирования
+logger.debug("Debug message")
+logger.info("Info message")
+logger.warning("Warning message")
+logger.error("Error message")
+logger.critical("Critical message")
